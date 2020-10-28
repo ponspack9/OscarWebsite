b81d9e24f0d2b8c4cb50d34dfa20a7a3cc6351b6
ponspack9 <ospoga9@gmail.com>
2020-10-28 20:07:09 +0100

    About

--- OldDocs/js/init.js
+++ OldDocs/js/init.js
@@ -3,6 +3,13 @@
 
     $('.sidenav').sidenav();
     M.AutoInit();
+      $('.dropdown-trigger').dropdown(
+        {
+          'hover':true,
+          'constrainWidth':false,
+          'coverTrigger':false,
+          'alignment': 'right',
+        });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
